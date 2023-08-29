14ee452d49f5a1584504ab066269f4ab2b2e4ccc
SatyaDevi-Amujala <133928428+SatyaDevi-Amujala@users.noreply.github.com>
2023-08-29 08:09:13 +0530

    Update Create.js

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -27,17 +27,18 @@ const Create = () => {
       
     }
     else{
-     
+     Id=201;
     }
     //Posting new data into rest API
     axios.post('https://jsonplaceholder.typicode.com/todos',{ userId:1,
+    id:Id,
     title:Title,
     completed:status}).then(
        
     (arr) => {console.log("JSONDATA",arr.data);
     //Adding newly created data at front of array
   data.unshift(arr.data);
-    //data[0].id=Id
+    data[0].id=Id
              });
 
   
